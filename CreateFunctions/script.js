7d2d91674e3592ea04147d349aa7565de4fa4146
Ömer <55637993+Omer975@users.noreply.github.com>
2019-11-29 22:21:54 +0300

    Update script.js

--- CreateFunctions/script.js
+++ CreateFunctions/script.js
@@ -1,1 +1,10 @@
+//function that will add the GIF to the page using only the ID as argument
+
+function showGif(giphyId) {
+  const imgUrl = `https://media.giphy.com/media/${giphyId}/giphy.gif`
+  document.write(`<img src="${imgUrl}" />`)
+}
+
+showGif('njYrp176NQsHS')
+showGif('NCPcywASJGggw')
 
