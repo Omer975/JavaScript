c4030617ae7e399c350ddde82ced71f629acda91
Ömer <55637993+Omer975@users.noreply.github.com>
2019-11-29 22:34:11 +0300

    Update script.js

--- BrowserEvents/EventonClick/script.js
+++ BrowserEvents/EventonClick/script.js
@@ -1,1 +1,7 @@
+// When clicking on a button will show an alert message
 
+function clickHandler () {
+  alert('You clicked the button!')
+}
+
+document.getElementById('btn1').addEventListener('click', clickHandler)
