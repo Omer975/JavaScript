6a9bac730c5c2c3c88fc1a73ee6adbaaeac437d4
Ömer <55637993+Omer975@users.noreply.github.com>
2019-11-29 22:12:06 +0300

    Update script.js

--- UseConditionals/script.js
+++ UseConditionals/script.js
@@ -1,1 +1,8 @@
-
+//This JS code ask your password if the password is "friend" shows a "welcome" message else show  "you shall not pass" message
+const password = prompt('Enter Password')
+var name = prompt('Enter your name')
+if (password === "friend") {
+  document.write("Welcome "  + name)
+ }else{
+  alert("you shall not pass")
+ }
