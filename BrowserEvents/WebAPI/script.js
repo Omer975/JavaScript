bda9a3ae3bf6ca8bbbc6a64249b6874af1139e2c
Ömer <55637993+Omer975@users.noreply.github.com>
2019-11-29 22:45:16 +0300

    Update script.js

--- BrowserEvents/WebAPI/script.js
+++ BrowserEvents/WebAPI/script.js
@@ -1,1 +1,11 @@
+//When clicking on a button then the button should become green
 
+function onClick () {
+  // Step1: get a reference to the button
+  const $btn1El=document.getElementById("btn1")
+  // Step2: add the class "clicked"
+  $btn1El.classList.toggle("clicked")
+}
+
+// Configure the event handler on the button
+addEventListener('click', onClick)
