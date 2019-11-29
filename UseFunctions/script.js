2ac8084279a05854161e6d0bedbc0b608e08145f
Ömer <55637993+Omer975@users.noreply.github.com>
2019-11-29 21:47:37 +0300

    Update script.js

--- UseFunctions/script.js
+++ UseFunctions/script.js
@@ -1,1 +1,6 @@
+document.write('Hello World')
+window.alert('This is a JS alert message')
+window.confirm('Do you want to quit?')
+var a= window.prompt('What\'s your name?')
+window.alert('Hello ' + a)
 
