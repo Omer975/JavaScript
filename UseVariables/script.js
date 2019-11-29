5d7660745c92f3e5dcc3f37bfd19ada6b1445158
Ömer <55637993+Omer975@users.noreply.github.com>
2019-11-29 21:54:52 +0300

    Update script.js

--- UseVariables/script.js
+++ UseVariables/script.js
@@ -1,1 +1,5 @@
-
+//takes full name and shows a custom welcome message in the page
+ 
+var name = prompt('Enter please your name?')
+var lastName = prompt('Enter please your last name?')
+document.write(`<h2>Welcome ${name} ${lastName}</h2>`)
